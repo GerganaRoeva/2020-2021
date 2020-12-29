89aeb840175a818193d01552896c4ace5bdd9cde
GerganaRoeva <gergana.roeva@gmail.com>
2020-12-29 16:11:41 +0200

    shell

--- os/shell/shell.c
+++ os/shell/shell.c
@@ -17,30 +17,54 @@ int main(int argc, char const *argv[])
         char *cmdline = NULL;
         size_t size = 0;
         int get_res = getline(&cmdline, &size, stdin);
-        if(get_res == -1) return 0;
+        if(get_res == -1) 
+        {
+            free(cmdline);
+            return -1;
+        }
 
         pid_t fork_res = fork();
 
-        if(fork_res == -1) perror("fork");
+        if(fork_res == -1)
+        {
+            free(cmdline);
+            perror("fork");
+            continue;
+        }
 
         if(fork_res == 0)
         {
             char ** arr;
             arr = parse_cmdline(cmdline);
 
-            // printf("%s", arr[0]);
-            
+            if(arr == NULL) return 0;
+
             int exec_res = execvp(arr[0], arr);
             
             if (exec_res == -1)
             {
                 perror(arr[0]);
+                free(cmdline);
+                free(arr);
+                exit(EXIT_FAILURE);
                 
             }
             free(arr);
+            free(cmdline);
         }
 
-        wait(NULL);
+        pid_t wait_res = wait(NULL);
+
+        
+        if (wait_res == -1)
+        {
+            free(cmdline);
+            perror("wait");
+            continue;
+        }
+
+        free(cmdline);
+
     }
     
     return 0;
@@ -54,22 +78,21 @@ char **parse_cmdline(const char *cmdline)
     int i = 0;
     char * pch;
 
-    pch = strtok(in_use ," \n");
-    char ** arr= malloc(sizeof(pch));
+	pch = strtok(in_use ," \n");
+	char ** arr= (char**)malloc(sizeof(char*));
+    
     while (pch != 0)
     {
-        arr = realloc(arr, (i+1)*sizeof(pch));
+        arr = (char**)realloc(arr, (i + 1)*sizeof(char*));
         arr[i] = malloc(strlen(pch) + 1); 
         strcpy(arr[i], pch);
-        // printf ("%s\n",arr[i]);
         pch = strtok (NULL, " \n");
         i++;
     }
 
-    arr[i] = realloc(arr[i], sizeof(NULL));
+    arr = realloc(arr, i * sizeof(char*) + sizeof(NULL));
     arr[i] = NULL;
 
     return arr;
 
 }
-
