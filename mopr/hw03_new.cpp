0a1cb1f7bb179aa1ec3b26d6a3df3f0d5978ab1a
GerganaRoeva <gergana.roeva@gmail.com>
2021-01-16 21:02:04 +0200

    hw03

--- mopr/hw03_new.cpp
+++ mopr/hw03_new.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <string.h>
 
 using namespace std;
 
-struct Point
-{
-    double x;
-    double y;
-};
+// struct Point
+// {
+//     double x;
+//     double y;
+// };
 
 void read(double arr[], const char * filename)
 {
@@ -17,30 +18,39 @@ void read(double arr[], const char * filename)
     string num_string;
     double num;
 
-    for (int i = 0; i < 3; i++)
+    for(int i = 0; info.eof() == 0; i++)
     {
         getline(info, num_string, ' ');
         num = stod(num_string);
-
-        int indx = 0;
-
-        // while(num_string[indx] != '\0')
-        // {
-        //     if(num_string[indx] == ' ')
-
-
-
-        // }
-
         arr[i] = num;
-        cout << arr[i]<< endl;
+        // cout << arr[i] << endl;
+    }
+}
+
+int board_coordinates_count(const char * filename)
+{
+    ifstream info;
+    info.open (filename);
+    string coordinates;
+
+    getline(info, coordinates);
+    int count = 0;
+    int i = 0;
+    while(coordinates[i] != '\0')
+    {
+        if(coordinates[i] == ' ') count++;
+        i++;
     }
 
+    return count;
 }
 
 int main(int argc, char const *argv[])
 {
-    double arr[11]; 
+    int num_coordinates = board_coordinates_count(argv[1]);
+    // cout << num_coordinates;
+    double arr[num_coordinates + 3];
+
 
     read(arr, argv[1]);
 }
