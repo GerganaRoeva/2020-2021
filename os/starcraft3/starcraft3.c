f84d2c5fad63d13269ea04a5b5f51ad1b2fa558a
GerganaRoeva <gergana.roeva@gmail.com>
2021-03-25 21:52:29 +0200

    sc3

--- os/starcraft3/starcraft3.c
+++ os/starcraft3/starcraft3.c
@@ -6,23 +6,14 @@
 
 #define THREADS_SCV 5
 
-pthread_mutex_t mutex_for_mine;
+pthread_mutex_t* mutex_for_mine;
 pthread_mutex_t mutex_for_bank;
-// pthread_mutex_t mutex_for_filmine;
+// pthread_mutex_t mutex_for_mapmineral;
 
-    int num_mines;
-    int mirals_bank = 0;
-    int soldiers_count = 0;
-    int* mine;
-
-struct info_SCV_t
-{
-    int SCV_index;
-    int num_mines;
-    int mirals_bank;
-    int soldiers_count;
-    int* mine;
-};
+int num_mines;
+int mirals_bank = 0;
+int soldiers_count = 0;
+int* mine;
 
 int are_mines_empty(int mines[], int num_mines)
 {
@@ -38,79 +29,79 @@ int are_mines_empty(int mines[], int num_mines)
 
 void* workers_job(void* arg)
 {
-    // struct info_SCV_t *info = (struct info_SCV_t*) arg;
     int* index = (int*)arg;
 
     int i = 1;
     int send;
 
-    // info->soldiers_count == 20 && 
-
     while(1)
     {
         if(i > num_mines) i = 1;
-
-        // printf("SCV %d is mining from mineral block %d\n", info->SCV_index, i);
-
-        int res_lock = pthread_mutex_trylock(&mutex_for_mine);
-        if(res_lock == 0)
+        if(mine[i - 1] > 0)
         {
-            printf("SCV %d is mining from mineral block %d\n", *index, i);
-            mine[i-1] = mine[i-1] - 8;
-            sleep(3);
-            if(mine[i] <= 8 && mine[i] > 0)
-            send = 500%8;
-            else if(mine[i] <= 0)
+        // int res_lock = pthread_mutex_trylock(&mutex_for_mine[i - 1]);
+            if(pthread_mutex_trylock(&mutex_for_mine[i - 1]) == 0)
             {
-                i++;
-                continue;
-            }
-            else send = 8;
-            pthread_mutex_unlock(&mutex_for_mine);
+                printf("SCV %d is mining from mineral block %d\n", *index, i);
+                sleep(3);
+                // sleep(1);
+                
+                if(mine[i - 1] < 8)
+                {
+                    send = mine[i - 1];
+                    mine[i - 1] = 0;
+                }
+                else
+                {
+                    mine[i - 1] -= 8;
+                    send = 8;
+                }
+                printf("SCV %d is transporting minerals\n", *index);
+                pthread_mutex_unlock(&mutex_for_mine[i - 1]);
 
-            printf("SCV %d is transporting minerals\n", *index);
-            sleep(2);
+                sleep(2);
+                // sleep(1);
 
-            // pthread_mutex_trylock(&mutex);
-            pthread_mutex_lock(&mutex_for_bank);
-            mirals_bank = mirals_bank + send;
-            pthread_mutex_unlock(&mutex_for_bank);
-            printf("SCV %d delivered minerals to the Command Center\n", *index);
-            printf("Map minerals %d, player minerals %d, SCVs %d, Marines %d\n", num_mines*500, mirals_bank, 5, soldiers_count);
+                pthread_mutex_lock(&mutex_for_bank);
+                mirals_bank += send;
+                printf("SCV %d delivered minerals to the Command Center\n", *index);
+                pthread_mutex_unlock(&mutex_for_bank);
 
-            sleep(2);
+                // pthread_mutex_lock(&mutex_for_mapmineral);
 
-            for (int z = 0; z < num_mines; z++)
-            {
-                printf("Mine %d, %d\n", z + 1, mine[z]);
+                // for (int z = 0; z < num_mines; z++)
+                // {
+                //     printf("Mine %d, %d\n", z + 1, mine[z]);
+                // //     map_minerals += mine[z];
+                // }
+
+                // pthread_mutex_unlock(&mutex_for_mapmineral);
+
+                // printf("bank: %d\n", mirals_bank);
+                // printf("soldiers_count: %d\n", soldiers_count);
             }
         }
         i++;
-        if(are_mines_empty(mine, num_mines)) break;
 
+        if(are_mines_empty(mine, num_mines) && soldiers_count == 20)
+        {
+            return NULL;    
+        }
     }
-
-    printf("Map minerals %d, player minerals %d, SCVs %d, Marines %d", num_mines*500, mirals_bank, 5, soldiers_count);
-
-    return NULL;    
 }
-
 
 
 int main(int argc, char const *argv[])
 {
+    // printf("test1\n");
+    int bonus_SCV = 0;
+
     num_mines = 2;
     if(argc > 1)
     {
         num_mines = atoi(argv[1]);
     }
 
-    // struct info_SCV_t info_for_worker;
-    // info_for_worker.num_mines = num_mines; 
-    // info_for_worker.mirals_bank = 0;
-    // info_for_worker.soldiers_count = 0;
-
-    // pthread_mutex_lock(&mutex_for_filmine);
     int mines[num_mines];
     for (size_t i = 0; i < num_mines; i++)
     {
@@ -118,55 +109,104 @@ int main(int argc, char const *argv[])
     }
     mine = mines;
 
-    // pthread_mutex_unlock(&mutex_for_filmine);
+    mutex_for_mine = malloc(THREADS_SCV*sizeof(pthread_mutex_t));
 
-
-    // info_for_worker.mine = mines;
-
-
-    pthread_t worker_thr[THREADS_SCV];
+    pthread_t* worker_thr = malloc(THREADS_SCV*sizeof(pthread_t));
     int index [THREADS_SCV];
     int err;
+    // printf("test1\n");
+
 
     pthread_mutex_init(&mutex_for_bank, NULL);
-    pthread_mutex_init(&mutex_for_mine, NULL);
-    // pthread_mutex_init(&mutex_for_filmine, NULL);
+    // printf("test1\n");
+
+    for (int i = 0; i < THREADS_SCV; i++)
+    {
+        // printf("test1\n");
+        pthread_mutex_init(&mutex_for_mine[i], NULL);
+    }
+
+    // pthread_mutex_init(&mutex_for_mapmineral, NULL);
+
+    // printf("test2\n");
 
     for(int i = 0; i < THREADS_SCV ; i++)
     {
         index[i] = i + 1;
-        // printf("SCV_index: %d\n", info_for_worker.SCV_index);
         
         if((err = pthread_create(&worker_thr[i], NULL, &workers_job, &index[i])))
         {
-            printf("error in thr: %s\n", strerror(err));
+            // printf("error in thr: %s\n", strerror(err));
+            perror("error in thr: ");
             return 1;
         }
     }
 
-    for(int i = 0; i < THREADS_SCV; i++)
+    char command;
+    int indexx;
+    while(soldiers_count < 20 && bonus_SCV < 180)
     {
-        if((err = pthread_join(worker_thr[i], NULL)))
+        command = getchar();
+        if((command == 'm' || command == 's') && mirals_bank < 50)
         {
+            printf("Not enough minerals.\n");
+        }
+        else if(command == 'm' && mirals_bank >= 50)
+        {
+            pthread_mutex_lock(&mutex_for_bank);
+            mirals_bank -= 50;
+            pthread_mutex_unlock(&mutex_for_bank);
+            sleep(1);
+            soldiers_count++;
+
+            printf("You wanna piece of me, boy?\n");
+        }
+        else if(command == 's' && mirals_bank >= 50){
+            pthread_mutex_lock(&mutex_for_bank);
+            mirals_bank -= 50;
+            pthread_mutex_unlock(&mutex_for_bank);
+            sleep(4);
+            bonus_SCV++;
+            worker_thr = realloc(worker_thr, (THREADS_SCV + bonus_SCV)*sizeof(pthread_t));
+            indexx = THREADS_SCV + bonus_SCV;
+
+            if((err = pthread_create(&worker_thr[THREADS_SCV + bonus_SCV - 1], NULL, &workers_job, &indexx)))
+            {
+                // printf("error in thr: %s\n", strerror(err));
+                perror("error in thr: ");
+                return 1;
+            }
+
+            printf("SCV good to go, sir.\n");
+        }
+    }        
+
+    
+    // pthread_mutex_destroy(&mutex_for_mapmineral);
+
+    printf("Map minerals %d, player minerals %d, SCVs %d, Marines %d\n", num_mines*500, mirals_bank, THREADS_SCV + bonus_SCV, soldiers_count);
+
+    free(mutex_for_mine);
+
+    for (int i = 0; i < THREADS_SCV; i++)
+        pthread_mutex_destroy(&mutex_for_mine[i]);
+
+    pthread_mutex_destroy(&mutex_for_bank);
+    // printf("bonus: %d", bonus_SCV);
+
+    for(int i = 0; i < (THREADS_SCV + bonus_SCV); i++)
+    {
+        err = pthread_join(worker_thr[i], NULL);
+        if(err != 0)
+        {
+            // perror("error in join");
+            // printf("i: %d\n", i);
             printf("error in join: %s\n", strerror(err));
+
             return 1;
         }
     }
-
-    // char command;
-    // command = getchar();
-
-    // if(command == 'm' && info_for_worker.mirals_bank >= 50)
-    // {
-    //     info_for_worker.soldiers_count++;
-    //     info_for_worker.mirals_bank = info_for_worker.mirals_bank - 50;
-    // }
-
-    pthread_mutex_destroy(&mutex_for_mine);
-    pthread_mutex_destroy(&mutex_for_bank);
-    // pthread_mutex_destroy(&mutex_for_filmine);
-
-    // printf("klkll");
+    free(worker_thr);
     
     return 0;
 }
