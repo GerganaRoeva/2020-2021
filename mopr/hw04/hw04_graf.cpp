02e1412dc16b25dc76d2ef765dfea57d8dac0b0f
GerganaRoeva <gergana.roeva@gmail.com>
2021-05-17 21:04:16 +0300

    read matrix from file

--- mopr/hw04/hw04_graf.cpp
+++ mopr/hw04/hw04_graf.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-int N = 0;
-
-void count_points(const char * filename)
+int count_points(const char * filename)
 {
     ifstream info;
     info.open (filename);
@@ -19,47 +18,70 @@ void count_points(const char * filename)
         count_points++;
     }
 
-    N = count_points;
+    return count_points;
 }
 
-void print(double A[][N])
-{
-	for(int i = 0; i < N + 1; ++i)
-	{
-		for(int j = 0; j < N; ++j)
-		{
-			cout << A[i][j] << " ";
-		}
-		cout << endl;
-	}
+// void print(double A[][N])
+// {
+// 	for(int i = 0; i < N + 1; ++i)
+// 	{
+// 		for(int j = 0; j < N; ++j)
+// 		{
+// 			cout << A[i][j] << " ";
+// 		}
+// 		cout << endl;
+// 	}
 
-}
+// }
 
-int shortest_start_fin()
-{
-    int st_point;
-    cout << "Starting point: ";
-    cin >> st_point;
-}
+// int shortest_start_fin()
+// {
+//     int st_point;
+//     cout << "Starting point: ";
+//     cin >> st_point;
+//     return 0;
+// }
 
-void read_file(double A[][N], const char * filename)
+void read_file(vector<vector<int> > matrix, const char * filename)
 {
+    ifstream info;
+    info.open (filename);
+
+    string num_string;
+    int num;
+
+    while (info.eof() == 0)
+    {
+        
+        for (size_t i = 0; i < matrix.size(); i++)
+        {
+            getline(info, num_string, ' ');
+            num = stoi(num_string);
+            matrix[i].push_back(num);
+            printf("%d ", num);
+        }
+        
+        
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    // count_points(argv[1]);
-
-    double matrix[][N] = {
-                        {0, 20, 15, 30, 55},
-                        {20, 0, 35, 25, 12},
-                        {15, 35, 0, 30, 55},
-                        {30, 25, 30, 0, 23},
-                        {55, 12, 55, 23, 0}
-                        };
+    
+    vector<vector<int> > matrix(count_points(argv[1]));
 
 
-    // read_file(matrix, argv[1]);
+
+    // double matrix[][N] = {
+    //                     {0, 20, 15, 30, 55},
+    //                     {20, 0, 35, 25, 12},
+    //                     {15, 35, 0, 30, 55},
+    //                     {30, 25, 30, 0, 23},
+    //                     {55, 12, 55, 23, 0}
+    //                     };
+
+
+    read_file(matrix, argv[1]);
     
 
     // printf("%d N", N);
