8c7d772904dda2e4cf8e450f38381c59dac12290
GerganaRoeva <gergana.roeva@gmail.com>
2021-05-29 12:16:48 +0300

    basic ls and -A

--- os/ls/ls.c
+++ os/ls/ls.c
@@ -15,6 +15,7 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <dirent.h>
+#include <errno.h>
 
 #define DT_UNKNOWN       0
 #define DT_FIFO          1
@@ -32,18 +33,55 @@
 
 int A = 0;
 
-void ls(int argc, char * const argv[])
+int only_ls(int argc, char * const argv[])
+{
+    for(int i = 1; i < argc; i++)
+    {
+        if(argv[i][0] != '-') return 0;
+    }
+    return 1;
+}
+
+void info()
+{
+
+}
+
+void ls(char * const name, int argc)
 {
     struct stat data;
 
-    for(int i = 1; i < argc; i++)
+    if(stat(name, &data) == -1)
     {
-        if (argv[i][0] == '-') continue;
+        const char* msg1 = "ls: cannot access ";
+        const char* msg2 = name;
 
-        if(stat(argv[i], &data) == -1)
+        char* msg;
+        msg = malloc(strlen(msg1)+1+strlen(msg2));
+        strcpy(msg, msg1);
+        strcat(msg, msg2);
+
+        perror(msg);
+    }
+
+    if(S_ISREG(data.st_mode ))    printf("- %s\n", name);
+    if(S_ISBLK(data.st_mode ))    printf("b %s\n", name);
+    if(S_ISCHR(data.st_mode ))    printf("c %s\n", name);
+    if(S_ISSOCK(data.st_mode))    printf("s %s\n", name);
+    if(S_ISFIFO(data.st_mode))    printf("p %s\n", name);
+    if(S_ISLNK(data.st_mode ))    printf("l %s\n", name);
+
+    if(S_ISDIR(data.st_mode))
+    {
+        DIR* dir;
+        struct dirent *dir_inf; 
+
+        if (argc > 2) printf("%s:\n", name);
+
+        if((dir = opendir(name)) == NULL)
         {
-            const char* msg1 = "ls: cannot access ";
-            const char* msg2 = argv[i];
+            const char* msg1 = "ls: cannot open directory ";
+            const char* msg2 = name;
 
             char* msg;
             msg = malloc(strlen(msg1)+1+strlen(msg2));
@@ -51,55 +89,22 @@ void ls(int argc, char * const argv[])
             strcat(msg, msg2);
 
             perror(msg);
-            continue;
         }
-
-        if(S_ISREG(data.st_mode ))    printf("- %s\n", argv[i]);
-        if(S_ISBLK(data.st_mode ))    printf("b %s\n", argv[i]);
-        if(S_ISCHR(data.st_mode ))    printf("c %s\n", argv[i]);
-        if(S_ISSOCK(data.st_mode))    printf("s %s\n", argv[i]);
-        if(S_ISFIFO(data.st_mode))    printf("p %s\n", argv[i]);
-        if(S_ISLNK(data.st_mode ))    printf("l %s\n", argv[i]);
-
-        if(S_ISDIR(data.st_mode))
+        else while((dir_inf = readdir(dir)) != NULL)
         {
-            DIR* dir;
-            struct dirent *dir_inf;
-    
-            if (i != 1) printf("\n");
-            printf("%s:\n", argv[i]);
+            if(A == 0 && dir_inf->d_name[0] == '.')                                continue;
+            if(!strcmp(dir_inf->d_name, ".") || !strcmp(dir_inf->d_name, ".."))    continue;
 
-            if((dir = opendir(argv[i])) == NULL)
-            {
-                const char* msg1 = "ls: cannot open directory ";
-                const char* msg2 = argv[i];
-
-                char* msg;
-                msg = malloc(strlen(msg1)+1+strlen(msg2));
-                strcpy(msg, msg1);
-                strcat(msg, msg2);
-
-                perror(msg);
-            }
-            else while((dir_inf = readdir(dir)) != NULL)
-            {
-                if(A == 0 && dir_inf->d_name[0] == '.')                                continue;
-                if(!strcmp(dir_inf->d_name, ".") || !strcmp(dir_inf->d_name, ".."))    continue;
-
-                if(dir_inf->d_type == DT_REG )      printf("- %s\n", dir_inf->d_name);
-                if(dir_inf->d_type == DT_BLK )      printf("b %s\n", dir_inf->d_name);
-                if(dir_inf->d_type == DT_CHR )      printf("c %s\n", dir_inf->d_name);
-                if(dir_inf->d_type == DT_SOCK)      printf("s %s\n", dir_inf->d_name);
-                if(dir_inf->d_type == DT_FIFO)      printf("p %s\n", dir_inf->d_name);
-                if(dir_inf->d_type == DT_LNK )      printf("l %s\n", dir_inf->d_name);
-                if(dir_inf->d_type == DT_DIR )      printf("d %s\n", dir_inf->d_name);
-            }
-
-            closedir(dir);
-
+            if(dir_inf->d_type == DT_REG )      printf("- %s\n", dir_inf->d_name);
+            if(dir_inf->d_type == DT_BLK )      printf("b %s\n", dir_inf->d_name);
+            if(dir_inf->d_type == DT_CHR )      printf("c %s\n", dir_inf->d_name);
+            if(dir_inf->d_type == DT_SOCK)      printf("s %s\n", dir_inf->d_name);
+            if(dir_inf->d_type == DT_FIFO)      printf("p %s\n", dir_inf->d_name);
+            if(dir_inf->d_type == DT_LNK )      printf("l %s\n", dir_inf->d_name);
+            if(dir_inf->d_type == DT_DIR )      printf("d %s\n", dir_inf->d_name);
         }
+        closedir(dir);
     }
-    
 }
 
 
@@ -117,8 +122,20 @@ int main(int argc, char * const argv[])
         } 
     }
 
-    ls(argc, argv);
+    for(int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == '-') continue;
+        if (i != 1) printf("\n");
+        
+        ls(argv[i], argc);
+        
+    }
 
+    if(only_ls(argc, argv)) 
+    {
+        char buff[100];
+        getcwd(buff, 100);
+        ls(buff, argc);
+    }
     return 0;
-
 }
